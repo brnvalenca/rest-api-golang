7e5263e0a0daf73e886896735b9fe91ae09703e4
Bruno Valenca <brunovalenca10@gmail.com>
2022-07-27 11:25:46 -0300

    controllers and repository folders

---
+++ controllers/dataHandler.go
@@ -0,0 +1,74 @@
+package controllers
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"rest-api/golang/exercise/domain/entities"
+	repository "rest-api/golang/exercise/repository/data"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(repository.Users)
+}
+
+func GetUsersById(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, user := range repository.Users {
+		if user.ID == params["id"] {
+			json.NewEncoder(w).Encode(user)
+		}
+	}
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var user entities.User
+
+	_ = json.NewDecoder(r.Body).Decode(&user) // Aqui eu decodifico o body da requisicao, que estará em JSON, contendo os dados do user
+	user.ID = strconv.Itoa(rand.Intn(1000000))
+	repository.Users = append(repository.Users, user)
+	json.NewEncoder(w).Encode(user) // Codifico a resposta guardada em w para JSON e mostro na tela.
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, user := range repository.Users {
+		if user.ID == params["id"] {
+			repository.Users = append(repository.Users[:index], repository.Users[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(repository.Users)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	for index, user := range repository.Users {
+
+		if user.ID == params["id"] {
+			repository.Users = append(repository.Users[:index], repository.Users[index+1:]...)
+			var user entities.User
+			user.ID = params["id"]
+			_ = json.NewDecoder(r.Body).Decode(&user)
+			repository.Users = append(repository.Users, user)
+			json.NewEncoder(w).Encode(repository.Users)
+		}
+	}
+}
---
+++ repository/data/dataRepository.go
@@ -0,0 +1,21 @@
+package repository
+
+import (
+	"rest-api/golang/exercise/domain/entities"
+)
+var Users []entities.User
+func MakeUsers(u []entities.User) []entities.User {
+
+	u = append(u, entities.BuildUser("1", "Bruno", "brnvalenca@gmail.com", "123"))
+	u = append(u, entities.BuildUser("2", "Luis", "lrcv@gmail.com", "453"))
+	u = append(u, entities.BuildUser("3", "Miranda", "mrnd@gmail.com", "897"))
+	u = append(u, entities.BuildUser("4", "Mahmed", "mahmed@gmail.com", "568"))
+
+	return u
+	
+}
+
+/* 
+	This layer is has the responsability of populate the slice of users with data so that can be 
+*/
+
